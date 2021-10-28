eb4897143a9138aa8d28077972c04acd5f06b04e
Olivier Guyot <olivier.guyot@camptocamp.com>
2021-10-28 10:49:26 +0200

    feat(api): expose useCache and sharedFetch utilities

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 export { default as WfsEndpoint } from './wfs/endpoint';
 export { default as WmsEndpoint } from './wms/endpoint';
+export { useCache } from './shared/cache';
+export { sharedFetch } from './shared/http-utils';
