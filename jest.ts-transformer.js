0a8a6d82edb015adc8c37cd121ca28b154e1be69
Olivia <olivia.guyot@camptocamp.com>
2023-12-25 16:24:16 +0100

    Fix tests & typescript error

--- jest.ts-transformer.js
+++ jest.ts-transformer.js
@@ -7,7 +7,7 @@ module.exports = {
   process(src, filename, options) {
     const dir = path.dirname(filename);
     const transformed = src.replace(
-      /new Worker\(['"](.+)['"]/,
+      /new Worker\(new URL\(['"](.+)['"], import\.meta\.url\)/,
       `new Worker('${dir}/$1'`
     );
     return tsJest
