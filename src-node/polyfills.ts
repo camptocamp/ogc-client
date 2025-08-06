8f38b57751176ba12a1e3f740371a8e5f2d4ef64
Olivia <olivia.guyot@camptocamp.com>
2025-08-06 13:47:18 +0200

    fix test setup for node
    This also means using a custom event target for handling events when
    no worker is available

--- src-node/polyfills.ts
+++ src-node/polyfills.ts
@@ -1,7 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 import 'regenerator-runtime/runtime';
-import { EventEmitter } from 'events';
 import { Blob } from 'buffer';
 import fetch from 'node-fetch';
 
@@ -12,17 +11,6 @@ if (!('fetch' in global)) {
   global.fetch = fetch;
 }
 
-// mimic window events
-const emitter = new EventEmitter();
-if (
-  !('addEventListener' in global) &&
-  !('removeEventListener' in global) &&
-  !('dispatchEvent' in global)
-) {
-  global.addEventListener = emitter.addListener.bind(emitter);
-  global.removeEventListener = emitter.removeListener.bind(emitter);
-  global.dispatchEvent = emitter.emit.bind(emitter);
-}
 if (!('CustomEvent' in global)) {
   global.CustomEvent = class {
     constructor(type, payload) {
