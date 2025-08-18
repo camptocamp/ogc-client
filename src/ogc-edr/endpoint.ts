1ddf88ff93f92e800714261e8df9fa56217eca46
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2025-08-18 09:46:51 -0400

    better datetime parsing

--- src/ogc-edr/endpoint.ts
+++ src/ogc-edr/endpoint.ts
@@ -1,12 +1,12 @@
 import OgcApiEndpoint from '../ogc-api/endpoint.js';
-import { fetchDocument } from '../ogc-api/link-utils.js';
+import { DateTimeParameter } from '../shared/models.js';
 import { DataQueryTypes, parseCollections } from '../shared/ogc-api/common.js';
-import { OgcEDRCollectionInfo, OgcEDRDocument } from './model.js';
+import { DateTimeParameterToEDRString } from './helpers.js';
+import { OgcEDRCollectionInfo } from './model.js';
 
 type wkt = string;
 
 export default class OgcApiEDREndpoint extends OgcApiEndpoint {
-  private edr_root_: Promise<OgcEDRDocument>;
 
   constructor(base_url: string) {
     super(base_url);
@@ -29,15 +29,15 @@ export default class OgcApiEDREndpoint extends OgcApiEndpoint {
       });
   }
 
-  async getArea(
+  async getAreaDownloadUrl(
     collectionId: string,
     coords: wkt,
     z?: string,
-    datetime?: string,
+    datetime?: DateTimeParameter,
     parameter_name?: string[],
     crs?: string,
     f?: string
-  ) {
+  ): Promise<string> {
     const collectionDoc = (await this.getCollectionDocument(
       collectionId
     )) as unknown as OgcEDRCollectionInfo;
@@ -46,25 +46,23 @@ export default class OgcApiEDREndpoint extends OgcApiEndpoint {
 
     url.searchParams.set('coords', coords);
     if (z !== undefined) url.searchParams.set('z', z);
-    if (datetime !== undefined) url.searchParams.set('datetime', datetime);
+    if (datetime !== undefined) url.searchParams.set('datetime', DateTimeParameterToEDRString(datetime));
     if (parameter_name != null) {
       url.searchParams.set('parameter-name', parameter_name.join(','));
     }
     if (crs !== undefined) url.searchParams.set('crs', crs);
     if (f !== undefined) url.searchParams.set('f', f);
-
-    const doc = await fetchDocument(url.toString());
-    return doc as unknown;
+    return url.toString();
   }
 
-  async getLocations(
+  async getLocationsDownloadUrl(
     collectionId: string,
     locationId?: string,
     parameter_name?: string[],
-    datetime?: string,
+    datetime?: DateTimeParameter,
     crs?: string,
     f?: string
-  ) {
+  ): Promise<string> {
     const collectionDoc = (await this.getCollectionDocument(
       collectionId
     )) as unknown as OgcEDRCollectionInfo;
@@ -73,34 +71,34 @@ export default class OgcApiEDREndpoint extends OgcApiEndpoint {
       url.searchParams.set('locationId', locationId);
     if (parameter_name !== undefined)
       url.searchParams.set('parameter-name', parameter_name.join(','));
-    if (datetime !== undefined) url.searchParams.set('datetime', datetime);
+    if (datetime !== undefined) url.searchParams.set('datetime', DateTimeParameterToEDRString(datetime));
     if (crs !== undefined) url.searchParams.set('crs', crs);
     if (f !== undefined) url.searchParams.set('f', f);
-    return (await fetchDocument(url.toString())) as unknown;
+    return url.toString();
   }
 
-  getCube(
+  async getCubeDownloadUrl(
     collectionId: string,
     bbox: number[],
     z?: string,
-    datetime?: string,
+    datetime?: DateTimeParameter,
     parameter_name?: string[],
     crs?: string,
     f?: string
-  ) {
+  ): Promise<string> {
     const collectionDoc = this.getCollectionDocument(
       collectionId
     ) as unknown as OgcEDRCollectionInfo;
     const url = new URL(collectionDoc.data_queries?.cube?.link.href);
     url.searchParams.set('bbox', bbox.join(','));
     if (z !== undefined) url.searchParams.set('z', z);
-    if (datetime !== undefined) url.searchParams.set('datetime', datetime);
+    if (datetime !== undefined) url.searchParams.set('datetime', DateTimeParameterToEDRString(datetime));
     if (parameter_name != null) {
       url.searchParams.set('parameter-name', parameter_name.join(','));
     }
     if (crs !== undefined) url.searchParams.set('crs', crs);
     if (f !== undefined) url.searchParams.set('f', f);
-    return fetchDocument(url.toString());
+    return url.toString();
   }
 
   // getTrajectory(collectionId: string) {}
---
+++ src/ogc-edr/helpers.ts
@@ -0,0 +1,24 @@
+import { DateTimeParameter } from "../shared/models.js";
+
+
+export function DateTimeParameterToEDRString(param: DateTimeParameter): string {
+  const format = (d: Date) => d.toISOString();
+
+  if (param instanceof Date) {
+    return format(param);
+  }
+
+  if ('start' in param && 'end' in param) {
+    return `${format(param.start)}/${format(param.end)}`;
+  }
+
+  if ('start' in param) {
+    return `${format(param.start)}/..`;
+  }
+
+  if ('end' in param) {
+    return `../${format(param.end)}`;
+  }
+
+  throw new Error('Invalid DateTimeParameter');
+}
